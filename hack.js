d05b5b37a7ca6ec6605c9902e157a065485a8f7b
Elias Peteri <elias.peteri@hotmail.com>
2021-12-28 17:08:05 +0200

    NS2 version of hack.script

---
+++ hack.js
@@ -0,0 +1,38 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+  const {
+    args,
+    getServerMaxMoney,
+    getServerMinSecurityLevel,
+    getServerMoneyAvailable,
+    getServerSecurityLevel,
+    grow,
+    hack,
+    sleep,
+    weaken
+  } = ns;
+  // target server
+  var target = args[0];
+
+  while (true) {
+    // if target's security level is greater than the target's minimum security level, weaken
+    if (
+      getServerSecurityLevel(target) >
+      getServerMinSecurityLevel(target) + 5
+    ) {
+      await weaken(target);
+    }
+    // else if target's money level is less than 90 % of max money, grow
+    else if (
+      getServerMoneyAvailable(target) <
+      getServerMaxMoney(target) * 0.9
+    ) {
+      await grow(target);
+    }
+    // else hack
+    else {
+      await hack(target);
+    }
+    await sleep(1000);
+  }
+}
