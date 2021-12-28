983f7e8653dc60b9837891d5854b8889e8c9290f
Elias Peteri <elias.peteri@hotmail.com>
2021-12-28 12:14:59 +0200

    separated main loop into its own script, and it's now called once upon startup.

---
+++ run.js
@@ -0,0 +1,179 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+  const hackingScript = '/scripts/hack.script';
+  var target = 'n00dles';
+  var targetUpdated = false;
+
+  const servers = [
+    'home',
+    'n00dles',
+    'foodnstuff',
+    'max-hardware',
+    'sigma-cosmetics',
+    'joesguns',
+    'zer0',
+    'neo-net',
+    'crush-fitness',
+    'rho-construction',
+    'aerocorp',
+    'global-pharm',
+    'aevum-police',
+    'hong-fang-tea',
+    'CSEC',
+    'phantasy',
+    'comptek',
+    'netlink',
+    'rothman-uni',
+    'harakiri-sushi',
+    'nectar-net',
+    'iron-gym',
+    'silver-helix',
+    'johnson-ortho',
+    'I.I.I.I',
+    'summit-uni',
+    'alpha-ent',
+    'snap-fitness',
+    'omnia',
+    'icarus',
+    'taiyang-digital',
+    'univ-energy',
+    'infocomm',
+    'deltaone',
+    'syscore',
+    'lexo-corp',
+    'omega-net',
+    'the-hub',
+    'zb-institute',
+    'millenium-fitness',
+    'snap-fitness',
+    'galactic-cyber',
+    'unitalife',
+    'defcomm',
+    'solaris',
+    'nova-med',
+    'zeus-med',
+    'zb-def',
+    'avmnite-02h',
+    'catalyst'
+  ]; // servers
+
+  const {
+    args,
+    exec,
+    getHackingLevel,
+    getPurchasedServers,
+    getServerMaxMoney,
+    getServerMaxRam,
+    getServerUsedRam,
+    getScriptRam,
+    getServerRequiredHackingLevel,
+    hasRootAccess,
+    killall,
+    scp,
+    sleep,
+    tprint
+  } = ns;
+
+  const debug = args[0];
+
+  // calculates max threads available
+  function calculateThreads(script, hostname) {
+    const [maxRam, usedRam, scriptRam] = [
+      getServerMaxRam(hostname),
+      getServerUsedRam(hostname),
+      getScriptRam(script)
+    ];
+    const availableRam = maxRam - usedRam;
+    if (debug) {
+      tprint(`Calculating amount of threads for: ${hostname}`);
+      tprint(`RAM available: ${availableRam}`);
+      tprint(`Script RAM required: ${scriptRam}`);
+    }
+    const threads = Math.floor(availableRam / scriptRam);
+    return threads;
+  } // calculateThreads()
+
+  // executes the hacking script on a server
+  function executeHack(server) {
+    const threads = calculateThreads(hackingScript, server);
+    if (debug) {
+      tprint(`server: ${server}`);
+      tprint(`threads: ${threads}`);
+      tprint(`target: ${target}`);
+    } // if-statement
+    if (threads > 0) {
+      exec(
+        hackingScript,
+        server,
+        threads,
+        target // target
+      );
+    } // if-statement
+  } // executeHack()
+
+  // main loop begins here
+  while (true) {
+    // calculates server with highest hacking level required, that the player has access to
+    const playerHackingLevel = getHackingLevel();
+    for (var i = 0; i < servers.length; i++) {
+      const server = servers[i];
+      const serverHackingLevel = getServerRequiredHackingLevel(server);
+      if (
+        serverHackingLevel > getServerRequiredHackingLevel(target) &&
+        playerHackingLevel >= serverHackingLevel &&
+        getServerMaxMoney(server) > getServerMaxMoney(target) &&
+        hasRootAccess(server)
+      ) {
+        tprint(`Target updated: ${target} -> ${server}.`);
+        target = server;
+        tprint(`Currently targeting ${target}.`);
+        targetUpdated = true;
+      } // if-statement
+    } // for-loop
+
+    /* starts hacking scripts on the purchased servers using the target */
+    for (var i = 0; i < getPurchasedServers().length; i++) {
+      const serv = getPurchasedServers()[i];
+
+      await scp(hackingScript, 'home', serv);
+
+      try {
+        executeHack(serv);
+      } catch (error) {
+        // try
+        tprint(String(error));
+        tprint(`server: ${serv}`);
+      } // catch
+    } // for-loop (purchased servers)
+
+    /* goes through the list of hacked servers and starts the hacking script on them one by one */
+    for (var i = 0; i < servers.length; i++) {
+      const serv = servers[i];
+      exec('/scripts/root.js', 'home', 1, serv, debug);
+      await scp(hackingScript, 'home', serv);
+
+      if (targetUpdated && serv != 'home') {
+        killall(serv);
+        executeHack(serv);
+      } // if-statement
+
+      if (
+        getServerMaxRam(serv) - getServerUsedRam(serv) > 0 &&
+        hasRootAccess(serv) &&
+        getHackingLevel() >= getServerRequiredHackingLevel(serv) &&
+        serv != 'home'
+      ) {
+        try {
+          killall(serv);
+          executeHack(serv);
+        } catch (error) {
+          // try
+          tprint(String(error));
+          tprint(`server: ${serv}`);
+        } // catch
+      } // if-statement
+    } // for-loop (hacked servers)
+    targetUpdated = false;
+    await sleep(60000);
+  } // while-loop
+}
--- startup.js
+++ startup.js
@@ -1,156 +1,26 @@
 /** @param {NS} ns **/
 export async function main(ns) {
   const debug = true;
-  const hackingScript = '/scripts/hack.script';
-  var target = 'n00dles';
-  var totalThreads = 0;
 
-  const servers = [
-    'home',
-    'n00dles',
-    'foodnstuff',
-    'max-hardware',
-    'sigma-cosmetics',
-    'joesguns',
-    'zer0',
-    'neo-net',
-    'crush-fitness',
-    'rho-construction',
-    'aerocorp',
-    'global-pharm',
-    'aevum-police',
-    'hong-fang-tea',
-    'CSEC',
-    'phantasy',
-    'comptek',
-    'netlink',
-    'rothman-uni',
-    'harakiri-sushi',
-    'nectar-net',
-    'iron-gym',
-    'silver-helix',
-    'johnson-ortho',
-    'I.I.I.I',
-    'summit-uni',
-    'alpha-ent',
-    'snap-fitness',
-    'omnia',
-    'icarus',
-    'taiyang-digital',
-    'univ-energy',
-    'infocomm',
-    'deltaone',
-    'syscore',
-    'lexo-corp',
-    'omega-net',
-    'the-hub',
-    'zb-institute',
-    'millenium-fitness',
-    'snap-fitness',
-    'galactic-cyber',
-    'unitalife',
-    'defcomm',
-    'solaris',
-    'nova-med',
-    'zeus-med',
-    'zb-def',
-    'avmnite-02h',
-    'catalyst'
-  ];
+  const { exec, serverExists, tprint } = ns;
 
-  const {
-    exec,
-    getHackingLevel,
-    getPurchasedServers,
-    getServerMaxMoney,
-    getServerMaxRam,
-    getServerUsedRam,
-    getScriptRam,
-    getServerRequiredHackingLevel,
-    hasRootAccess,
-    scp,
-    serverExists,
-    tprint
-  } = ns;
-
+  if (debug) tprint('Attempting to start /scripts/purchaseServers.js');
   // Start buying servers
   exec('/scripts/purchaseServers.js', 'home', 1, 2048);
 
+  if (debug) tprint('Attempting to start /scripts/bitburner/custom_stats.js');
   // Start custom stats
   exec('/scripts/bitburner/custom_stats.js', 'home');
 
-  // calculates max threads available
-  function calculateThreads(script, hostname) {
-    const threads = Math.floor(
-      (getServerMaxRam(hostname) - getServerUsedRam(hostname)) /
-        getScriptRam(script)
-    );
-    totalThreads += threads;
-    return threads;
-  }
-
-  // calculates server with highest hacking level required, that the player has access to
-  const playerHackingLevel = getHackingLevel();
-  for (var i = 0; i < servers.length; i++) {
-    const server = servers[i];
-    const serverHackingLevel = getServerRequiredHackingLevel(server);
-    if (
-      serverHackingLevel > getServerRequiredHackingLevel(target) &&
-      playerHackingLevel >= serverHackingLevel &&
-      getServerMaxMoney(server) > getServerMaxMoney(target) &&
-      hasRootAccess(server)
-    ) {
-      target = server;
-    }
-  }
-  tprint(`Currently targeting ${target}.`);
-
-  // Monitor the target
-  if (!debug) exec('/scripts/bitburner/monitor.js', 'home', 1, target);
-
-  /* starts hacking scripts on the purchased servers using a random destination from the server list */
-  for (var i = 0; i < getPurchasedServers().length; i++) {
-    const serv = getPurchasedServers()[i];
-
-    await scp(hackingScript, 'home', serv);
-
-    try {
-      exec(
-        hackingScript,
-        serv,
-        calculateThreads(hackingScript, serv),
-        target // target
-      );
-    } catch (error) {
-      tprint(String(error));
-    }
-  }
-
-  /* goes through the list of hacked servers and starts the hacking script on them one by one */
-  for (var i = 0; i < servers.length; i++) {
-    const serv = servers[i];
-    exec('/scripts/root.js', 'home', 1, serv, debug);
-    await scp(hackingScript, 'home', serv);
-
-    if (
-      getServerMaxRam(serv) - getServerUsedRam(serv) > 0 &&
-      hasRootAccess(serv) &&
-      getHackingLevel() >= getServerRequiredHackingLevel(serv)
-    ) {
-      exec(
-        hackingScript, // hacking script
-        serv, // server to hack on
-        calculateThreads(hackingScript, serv), // number of threads available
-        target // target, normally just the host itself
-      );
-    }
-  }
-  tprint(`Created ${totalThreads} new threads.`);
+  if (debug) tprint('Attempting to start /scripts/run.js');
+  // Start main program that handles hacking and allocating servers
+  exec('/scripts/run.js', 'home', 1, debug);
 
   // (Re)start Hacknet server
   if (serverExists('hacknet')) {
     ns.killall('hacknet');
     await ns.scp('/scripts/hacknet.js', 'home', 'hacknet');
+    if (debug) tprint('Attempting to start /scripts/hacknet.js');
     ns.exec('/scripts/hacknet.js', 'hacknet');
   }
 }
