e1ee5389669239f194d947f749d168d382e2b4a8
Elias Peteri <elias.peteri@hotmail.com>
2021-12-28 17:08:23 +0200

    debug mode for server purchase script and fixed a logic error

--- purchaseServers.js
+++ purchaseServers.js
@@ -7,41 +7,54 @@ export async function main(ns) {
     getPurchasedServerLimit,
     getServerMoneyAvailable,
     getPurchasedServerCost,
+    print,
     purchaseServer,
     serverExists,
     sleep,
     tprint
   } = ns;
 
-  disableLog('sleep');
-  disableLog('getServerMoneyAvailable');
+  const debug = args[1];
 
-  var ram = 8;
-  var index = 0;
-  var hacknetBought = false;
+  if (!debug) {
+    disableLog('sleep');
+    disableLog('getServerMoneyAvailable');
+  }
+
+  let ram = 8;
+  let hacknetBought = false;
+  const numServers = 24;
   args[0] ? (ram = args[0]) : (ram = 8);
-  if (getPurchasedServers.length >= getPurchasedServerLimit()) {
+  if (getPurchasedServers().length >= getPurchasedServerLimit()) {
     tprint('Max number of servers already purchased.');
   }
   if (!serverExists('hacknet')) {
     purchaseServer('hacknet', 8);
     tprint('Purchased a server to run hacknet.');
     hacknetBought = true;
+  } else {
+    hacknetBought = true;
   }
   while (getPurchasedServers().length < getPurchasedServerLimit()) {
+    if (debug) {
+      print(`hacknetBought: ${hacknetBought}`);
+      print(`server money: ${getServerMoneyAvailable('home')}`);
+      print(`server cost: ${getPurchasedServerCost(ram) * numServers}`);
+    }
     if (
       hacknetBought &&
-      getServerMoneyAvailable('home') >= getPurchasedServerCost(ram)
+      getServerMoneyAvailable('home') >=
+        getPurchasedServerCost(ram) * numServers
     ) {
-      purchaseServer(`pserv-${index}`, ram);
-      print(
-        `Purchased server pserv-${index} with ${ram}GB of RAM for \$${getPurchasedServerCost(
-          ram
-        )}.`
-      );
-      index++;
+      for (let i = 0; i < numServers; i++) {
+        purchaseServer(`pserv-${i}`, ram);
+        print(
+          `Purchased server pserv-${i} with ${ram}GB of RAM for \$${getPurchasedServerCost(
+            ram
+          )}.`
+        );
+      }
     }
-
     await sleep(1000);
   }
   tprint('Finished buying servers.');
--- startup.js
+++ startup.js
@@ -4,17 +4,13 @@ export async function main(ns) {
 
   const { exec, serverExists, tprint } = ns;
 
-  if (debug) tprint('Attempting to start /scripts/purchaseServers.js');
   // Start buying servers
-  exec('/scripts/purchaseServers.js', 'home', 1, 2048);
+  if (debug) tprint('Attempting to start /scripts/purchaseServers.js');
+  exec('/scripts/purchaseServers.js', 'home', 1, 2048, false);
 
-  if (debug) tprint('Attempting to start /scripts/bitburner/custom_stats.js');
   // Start custom stats
+  if (debug) tprint('Attempting to start /scripts/bitburner/custom_stats.js');
   exec('/scripts/bitburner/custom_stats.js', 'home');
-
-  if (debug) tprint('Attempting to start /scripts/run.js');
-  // Start main program that handles hacking and allocating servers
-  exec('/scripts/run.js', 'home', 1, debug);
 
   // (Re)start Hacknet server
   if (serverExists('hacknet')) {
@@ -23,4 +19,8 @@ export async function main(ns) {
     if (debug) tprint('Attempting to start /scripts/hacknet.js');
     ns.exec('/scripts/hacknet.js', 'hacknet');
   }
+
+  // Start main program that handles hacking and allocating servers
+  if (debug) tprint('Attempting to start /scripts/run.js');
+  exec('/scripts/run.js', 'home', 1, debug);
 }
